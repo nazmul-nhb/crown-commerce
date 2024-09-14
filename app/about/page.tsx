cf834e0f8cca2855155810a9391168fe08a7e1ff
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-09-14 19:38:47 +0600

    explored layout

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>About Page</p>
+            <Link href={'/'}>Back</Link>
+        </div>
+    );
+};
+
+export default About;
